a8c0fd7f8629becd323a4db0435cd3401556c8f2
nhani05 <nhanlexuan05@gmail.com>
2025-10-03 07:58:10 +0700

    first-commit-init-repo

---
+++ src/main/java/com/javaweb/project/model/BlogDTO.java
@@ -0,0 +1,49 @@
+package com.javaweb.project.model;
+
+public class BlogDTO {
+    private Long id;
+    private String title;
+    private String author;
+    private String createdAt;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
